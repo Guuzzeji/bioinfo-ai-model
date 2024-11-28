b134a952ac1acb9ae1b7d34b38586f171ab491a4
Guuzzeji <5lh8dl440f@gmail.com>
2024-11-27 21:48:50 -0800

    Adding in deepgo tooling

--- tooling/deepgo.py
+++ tooling/deepgo.py
@@ -1,2 +1,17 @@
 # https://deepgo.cbrc.kaust.edu.sa/deepgo/api/create
 
+import requests
+
+BASE_URL_ENDPOINT = "https://deepgo.cbrc.kaust.edu.sa/deepgo/api/create"
+
+def deepgo_predict(seq: str) -> list[dict]:
+    payload = {
+        "version": "1.0.21",
+        "data_format": "enter",
+        "data": seq,
+        "threshold": 0.3
+    }
+    response = requests.post(BASE_URL_ENDPOINT, json=payload)
+
+    # Going from predictions[0]/functions[2]"Molecular Function"/functions/* (getting all terms)
+    return [{"goterm": x[0], "about": x[1], "score": x[2]} for x in response.json()["predictions"][0]["functions"][1]["functions"]]
